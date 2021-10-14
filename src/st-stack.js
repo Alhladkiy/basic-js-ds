a3b8d767807b8724a03a9c6779cd25fca1997119
Alhladkiy <Alhladkiy@gmail.com>
2021-10-14 22:03:03 +0300

    feat: add st-stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement the Stack with a given interface via array.
  *
@@ -7,28 +5,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * const stack = new Stack();
  *
  * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
+ * stack.peek(); // returns the peek, but doesn't delete it, // returns 1
  * stack.pop(); // returns the top element from stack and deletes it, returns 1
  * stack.pop(); // undefined
  *
  */
 
+module.exports = class Stack {
+    stack = [];
 
- module.exports = class Stack {
-  push(element) {
- 
+    push(element) {
+        this.stack.push(element);
+    }
 
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    pop() {
+        return  this.stack.pop();
+         
+    }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    peek() {
+        return this.stack[this.stack.length - 1];
+    }
 }
